396eec977a6a975f453a24a224f5b6a266344dc6
Renato <renato.diogenes@devsquad.com>
2024-01-28 12:46:52 -0300

    Avoid creating duplicated foreign columns

--- database/migrations/2024_01_28_144052_create_sales_table.php
+++ database/migrations/2024_01_28_144052_create_sales_table.php
@@ -12,16 +12,15 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table): void {
             $table->id();
-            $table->foreignIdFor(User::class);
-            $table->foreignIdFor(Fruit::class);
+
+            $foreignUser  = $table->foreignIdFor(User::class);
+            $foreignFruit = $table->foreignIdFor(Fruit::class);
+
             $table->unsignedSmallInteger('quantity');
             $table->unsignedDecimal('value', 6, 2);
             $table->enum('discount', [0, 5, 10, 15, 20, 25]);
             $table->timestamp('sold_at');
             $table->timestamps();
-
-            $foreignUser  = $table->foreignId('user_id');
-            $foreignFruit = $table->foreignId('fruit_id');
 
             $foreignUser->constrained();
             $foreignFruit->constrained();
