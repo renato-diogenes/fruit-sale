dfcfd9a36e3eb47c448aee53e78c1b7a9a4d9c6e
Renato <renato.diogenes@devsquad.com>
2024-07-09 23:33:01 -0300

    Create a column to store the user role

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -8,4 +8,15 @@ enum Role
 {
     case MANAGER;
     case SELLER;
+
+    /**
+     * @return string[]
+     */
+    public static function toArray(): array
+    {
+        return array_map(
+            fn (self $role): string => $role->name,
+            self::cases()
+        );
+    }
 }
