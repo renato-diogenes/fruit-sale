8a07b17aa425bf5d7ec7b425de0ec747d718ae0a
Renato <renato.diogenes@devsquad.com>
2026-01-03 20:27:48 -0300

    Fix pint

--- tests/Feature/Livewire/SellFruitTest.php
+++ tests/Feature/Livewire/SellFruitTest.php
@@ -4,9 +4,7 @@ namespace Tests\Feature\Livewire;
 
 use App\Livewire\SellFruit;
 use App\Models\Fruit;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
 
